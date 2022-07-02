1aa520a8a5ae07669e17c3e706675d9c521f85f6
matheus coelho <matheus.coelho@outlook.com>
2022-07-02 12:49:18 -0300

    mongo uri

--- src/Database/mongodb.js
+++ src/Database/mongodb.js
@@ -2,10 +2,8 @@ import mongodb from "mongodb"
 import dotenv from "dotenv"
 
 dotenv.config()
-const srv = process.env.MONGO_SRV
-const pss = process.env.MONGO_PASS
 
-const dburl = `mongodb+srv://${srv}:${pss}@cluster0.lnxaw.mongodb.net/?retryWrites=true&w=majority`
+const dburl = process.env.MONGO_URI
 
 export function getConn() {
   return new mongodb.MongoClient(dburl)
